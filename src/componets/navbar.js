01d5d7072da985a01795ad0a23c7ae0feac7db83
NoahKKA <noahhhhh.a@gmail.com>
2023-12-08 01:10:36 -0800

    basic navbar completed still needs styling

--- src/componets/navbar.js
+++ src/componets/navbar.js
@@ -1,7 +1,17 @@
-export default function Navbar(){
-    return(
-        <div>
-            navbar
-        </div>
-    )
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function MainNavbar() {
+  return (
+    <Navbar >
+        <Navbar.Brand href="#home" style={{marginLeft:"1rem"}}>BudgetWise</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Community</Nav.Link>
+          <Nav.Link href="#pricing">FAQS</Nav.Link>
+        </Nav>
+        <Nav.Link href="#pricing">John Doe</Nav.Link>
+    </Navbar>
+  );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,13 @@
 import LeftColumn from "../componets/leftColumn";
 import MainColumn from "../componets/mainColumn";
-import Navbar from "../componets/navbar";
+import MainNavbar from "../componets/navbar";
 import RightColumn from "../componets/rightColumn";
 
 export default function Homepage(){
     return(
         <div>
-            <Navbar/>
-            <div className="container">
+            <MainNavbar/>
+            <div className="main-container">
                 <LeftColumn/>
                 <MainColumn/>
                 <RightColumn/>
