ea50385023ad54df9fd1b441bb0475e34cc5f02d
NoahKKA <noahhhhh.a@gmail.com>
2023-12-09 23:15:39 -0800

    finished leftColumn to have the 6 categories and styling also, made subtle changes to navbar to move text in the correct space

--- src/componets/leftColumn.js
+++ src/componets/leftColumn.js
@@ -1,7 +1,45 @@
-export default function LeftColumn(){
-    return(
-        <div className="left-column">
-            ddd
+import React, { useState } from "react";
+
+export default function LeftColumn() {
+  const [selectedText, setSelectedText] = useState(null);
+
+  const handleTextClick = (index) => {
+    setSelectedText(index);
+  };
+
+  const leftColumnArray = ["Overview", "Budget", "Transactions", "Trends", "Savings Goals", "Alerts"]
+
+  const getTextStyle = (index) => {
+    return {
+      cursor: "pointer",
+      padding: "8px 4px",
+      marginLeft: "4px",
+      display: "flex",
+      alignItems:"left"
+    };
+  };
+
+  const getBorderStyle = (index) => {
+    return {
+      borderLeft:
+        index === selectedText ? "2px solid black" : "2px solid transparent",
+        paddingLeft: "4px"
+    };
+  };
+
+  return (
+    <div className="left-column">
+    <div className="mt-5">
+      {leftColumnArray.map((_, index) => (
+        <div
+          key={index}
+          style={getTextStyle(index)}
+          onClick={() => handleTextClick(index)}
+        >
+          <div style={getBorderStyle(index)}>{leftColumnArray[index]}</div>
         </div>
-    )
+      ))}
+    </div>
+    </div>
+  );
 }
--- src/componets/navbar.js
+++ src/componets/navbar.js
@@ -4,14 +4,14 @@ import Navbar from "react-bootstrap/Navbar";
 
 export default function MainNavbar() {
   return (
-    <Navbar >
-        <Navbar.Brand href="#home" style={{marginLeft:"1rem"}}>BudgetWise</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Community</Nav.Link>
-          <Nav.Link href="#pricing">FAQS</Nav.Link>
+    <Navbar className="custom-navbar">
+        <Navbar.Brand href="#home" style={{marginLeft:"4rem", fontWeight: "bold", marginRight: '7rem'}} className="navbar-custom-text">BudgetWise</Navbar.Brand>
+        <Nav className="me-auto" >
+          <Nav.Link href="#home" className="navbar-custom-text ml-auto">Home</Nav.Link>
+          <Nav.Link href="#features"className="navbar-custom-text">Community</Nav.Link>
+          <Nav.Link href="#pricing" className="navbar-custom-text">FAQS</Nav.Link>
         </Nav>
-        <Nav.Link href="#pricing">John Doe</Nav.Link>
+        <Nav.Link href="#pricing" className="navbar-custom-text" style={{marginRight: '2rem', fontWeight: "bold"}}>John Doe</Nav.Link>
     </Navbar>
   );
 }
